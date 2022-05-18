6367acc519af7eea5b41b31b38619d6ba28e9328
beacor004 <99179898+beacor004@users.noreply.github.com>
2022-05-18 09:49:23 +0200

    classe articoli attributi e costruttore

---
+++ AS2122_4E_INF_Cordioli_GestioneArticoli.Library/Articoli.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AS2122_4E_INF_Cordioli_GestioneArticoli.Library
+{
+    public class Articoli
+    {
+        public string codice;
+        public string Codice { get { return Codice; } }
+
+        public string descrizione;
+        public string Descrizione { get { return Descrizione; } }
+
+        public double prezzo;
+        public string Prezzo { get { return Prezzo; } }
+
+        public string unita;
+        public string Unita { get { return Unita; } }
+
+        public Articoli(string codice, string descrizione, double prezzo, string unita)
+        {
+            this.codice = codice;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.unita = unita;
+        }
+
+    }
+}
--- AS2122_4E_INF_Cordioli_GestioneArticoli.Library/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AS2122_4E_INF_Cordioli_GestioneArticoli.Library
-{
-    public class Class1
-    {
-    }
-}
